79593e24f5491920705b4f5a3fb948f2fa580fc2
Sid <104507303+SiddheshVPatil@users.noreply.github.com>
2025-02-09 22:13:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const path = require('path')
 const { Server} = require('socket.io')
 const cors = require('cors')
 app.use(cors())
