419bc0557ada66b0f1059f498ce0c55ef637ec46
Sid <104507303+SiddheshVPatil@users.noreply.github.com>
2025-02-09 22:20:32 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import io from 'socket.io-client'
 import Chat from './chat'
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("http://localhost:5000")
 
 
 
